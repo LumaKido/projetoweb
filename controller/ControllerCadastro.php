4117bd92c171a0d0b6ab5eb218b024b39197580b
LumaKido <89153095+LumaKido@users.noreply.github.com>
2021-10-26 10:59:58 -0300

    adicionando delete

--- controller/ControllerCadastro.php
+++ controller/ControllerCadastro.php
@@ -1,5 +1,6 @@
 <?php
-require_once("../model/cadastro.php");
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once("$root/projetoweb/model/cadastro.php");
 
 class ControllerCadastro{
 
@@ -7,7 +8,12 @@ class ControllerCadastro{
 
     public function __construct(){
         $this->cadastro = new Cadastro();
-        $this->incluir();
+        if(isset($_GET['funcao']) && $_GET['funcao'] == "cadastro"){
+            $this->incluir();
+        }
+        else if(isset($_GET['funcao']) && $_GET['funcao'] == "editar"){
+            $this->editar($_GET['id']);
+        }
     }
 
     private function incluir(){
@@ -23,5 +29,35 @@ class ControllerCadastro{
             echo "<script>alert('Erro ao gravar registro!');</script>";
         }
     }
+
+    public function listar($id){
+        return $result = $this->cadastro->listar($id);
+    }
+
+    public function editar($id){
+        $this->cadastro->setId($id);
+        $this->cadastro->setNome($_POST['txtNome']);
+        $this->cadastro->setTelefone($_POST['txtTelefone']);
+        $this->cadastro->setOrigem($_POST['txtOrigem']);
+        $this->cadastro->setData_contato(date('Y-m-d',strtotime($_POST['txtDataContato'])));
+        $this->cadastro->setObservacao($_POST['txtObservacao']);
+        $result = $this->cadastro->editar();
+        if($result >= 1){
+            echo "<script>alert('Registro alterado com sucesso!');document.location='../consultar.php'</script>";
+        }else{
+            echo "<script>alert('Erro ao alterar o registro!');</script>";
+        }
+    }
+
+    public function excluir($id){
+        $result = $this->cadastro->excluir($id);
+        if($result >= 1){
+            echo "<script>alert('Registro excluido com sucesso!'),document.location='consultar.php'</script>";
+        }
+        else{
+            echo "<script>alert('Erro ao excluir o registro!');</script>";
+        }
+    }
 }
 new ControllerCadastro();
+?>
